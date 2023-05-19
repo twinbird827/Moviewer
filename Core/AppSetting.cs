fe8eb08d5a356eac654e6164cd6d61fee0ef9d56
horie <horie@able-biott.co.jp>
2023-05-19 18:52:59 +0900

    initial-0519

---
+++ Core/AppSetting.cs
@@ -0,0 +1,23 @@
+﻿using TBird.Core;
+
+namespace Moviewer.Core
+{
+    public class AppSetting : JsonBase<AppSetting>
+    {
+        private const string _path = @"lib\app-setting.json";
+
+        public static AppSetting Instance
+        {
+            get => _Instance = _Instance ?? new AppSetting();
+        }
+        private static AppSetting _Instance;
+
+        public AppSetting() : base(_path)
+        {
+            if (!Load())
+            {
+
+            }
+        }
+    }
+}
---
+++ Core/MenuType.cs
@@ -0,0 +1,30 @@
+﻿namespace Moviewer.Core
+{
+    public enum MenuType
+    {
+        NicoSearch,
+
+        NicoRanking,
+
+        NicoTemporary,
+
+        NicoHistory,
+
+        NicoFavorite,
+
+        NicoFavoriteDetail,
+
+        NicoChromium,
+
+        TubeSearch,
+
+        TubeRanking,
+
+        TubeTemporary,
+
+        TubeHistory,
+
+        TubeFavorite,
+
+    }
+}
