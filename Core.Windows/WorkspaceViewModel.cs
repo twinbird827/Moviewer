232f813065b7fad82337d666a3a83efc3d8b39f7
able-khorie <horie@able-biott.co.jp>
2023-06-04 15:39:30 +0900

    各画面用の共通定義クラスを追加

---
+++ Core.Windows/WorkspaceViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TBird.Wpf;
+
+namespace Moviewer.Core.Windows
+{
+    public class WorkspaceViewModel : BindableBase
+    {
+    }
+}
